9676e0de31d12f493ce9b149c69540d72b6b5997
kedson25 <matheuslite0333@gmail.com>
2025-01-04 16:29:09 -0300

    Gitgnore atualizado

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,15 +1,16 @@
 import { initializeApp, getApps } from 'firebase/app';
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 import { getFirestore, doc, setDoc } from 'firebase/firestore';
+import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID, FIREBASE_MEASUREMENT_ID } from 'react-native-dotenv';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCfpBmn3cdKP9vaGrDzKCB7oRPMSMx02tA",
-  authDomain: "ecooy-5b791.firebaseapp.com",
-  projectId: "ecooy-5b791",
-  storageBucket: "ecooy-5b791.firebasestorage.app",
-  messagingSenderId: "824859587278",
-  appId: "1:824859587278:web:9a6b5a4485af41e70dd69f",
-  measurementId: "G-LDCXYXPEXF"
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID
 };
 
 let app;
